c0832574d0641629cccd90592c90f0534d01caf9
Matthew Sharps <mds9637@email.vccs.edu>
2025-01-31 11:55:10 -0500

    w

--- Chapter2Excercise3W.cpp
+++ Chapter2Excercise3W.cpp
@@ -1,17 +1,20 @@
 /*
+File Name: Chapter1Excercise3W.cpp
+Developer: Matthew Sharps
+Date: January 31, 2025
 
-
-
+Requirements:
 
 
 */
 
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   cout << "Hello World!\n";
 }
 
 
